000e9a1c6c17ba2ec8aefef196e7b9e09af95314
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-02-25 05:15:23 +0100

    feat: simple test page

--- wiki/cmd/main.go
+++ wiki/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 type Page struct {
@@ -9,6 +10,23 @@ type Page struct {
 	Body []byte
 }
 
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
 func main() {
-	fmt.Println("Hello, Wiki!")
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a simple Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }

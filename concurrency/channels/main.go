f588dcf607bcb186a6e8fca98a01d686d212947c
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-03-15 11:26:09 +0100

    refactor: fan in with select

--- concurrency/channels/main.go
+++ concurrency/channels/main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 )
 
+
 func boring(msg string) <- chan string {
 	c := make(chan string)
 	go func() {
@@ -19,8 +20,15 @@ func boring(msg string) <- chan string {
 
 func fanIn(input1, input2 <- chan string) <- chan string {
 	c := make(chan string)
-	go func() { for { c <- <-input1} }()
-	go func() { for { c <- <-input2} }()
+	go func() { 
+		for {
+			select {
+			case s := <- input1: c <- s
+			case s := <- input2: c <- s
+			}
+		}
+
+	 }()
 	return c
 }
 
